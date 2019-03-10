b04942dc0fb7a964948e060e60cb25213f3ab0ca
max <imloama@gmail.com>
2019-03-10 21:32:48 +0800

    feat: gradle config

---
+++ projects/src/main/java/hoopoe/projects/ProjectsApplication.java
@@ -0,0 +1,13 @@
+package hoopoe.bi;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
