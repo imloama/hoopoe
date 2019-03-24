a141c21ff97f19149b38bb9c22776dfd4324ed03
max <imloama@gmail.com>
2019-03-24 21:51:55 +0800

    feat: users ui unfinished

---
+++ web/src/views/sys/users.vue
@@ -0,0 +1,101 @@
+/**
+ * 用户界面，展示用户列表，可以新增、修改、删除用户，查询时，无法查询到自身
+ */
+<template>
+  <a-card :bordered="false" class="card-area">
+    <div :class="advanced ? 'search' : null">
+      <!-- 搜索区域 -->
+      <a-form layout="horizontal">
+        <a-row >
+        <div :class="advanced ? null: 'fold'">
+            <a-col :md="12" :sm="24" >
+              <a-form-item
+                label="用户名"
+                :labelCol="{span: 4}"
+                :wrapperCol="{span: 18, offset: 2}">
+                <a-input v-model="queryParams.username"/>
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24" >
+              <a-form-item
+                label="部门"
+                :labelCol="{span: 4}"
+                :wrapperCol="{span: 18, offset: 2}">
+                <dept-input-tree @change="handleDeptChange"
+                                 ref="deptTree">
+                </dept-input-tree>
+              </a-form-item>
+            </a-col>
+          <template v-if="advanced">
+            <a-col :md="12" :sm="24" >
+              <a-form-item
+                label="创建时间"
+                :labelCol="{span: 4}"
+                :wrapperCol="{span: 18, offset: 2}">
+                <range-date @change="handleDateChange" ref="createTime"></range-date>
+              </a-form-item>
+            </a-col>
+          </template>
+        </div>
+          <span style="float: right; margin-top: 3px;">
+            <a-button type="primary" @click="search">查询</a-button>
+            <a-button style="margin-left: 8px" @click="reset">重置</a-button>
+             <a @click="toggleAdvanced" style="margin-left: 8px">
+              {{advanced ? '收起' : '展开'}}
+              <a-icon :type="advanced ? 'up' : 'down'" />
+            </a>
+          </span>
+        </a-row>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+        <a-button type="primary" ghost @click="add" v-hasPermission="'user:add'">新增</a-button>
+        <a-button @click="batchDelete" v-hasPermission="'user:delete'">删除</a-button>
+        <a-dropdown v-hasAnyPermission="'user:reset','user:export'">
+          <a-menu slot="overlay">
+            <a-menu-item v-hasPermission="'user:reset'" key="password-reset" @click="resetPassword">密码重置</a-menu-item>
+            <a-menu-item v-hasPermission="'user:export'" key="export-data" @click="exportExcel">导出Excel</a-menu-item>
+          </a-menu>
+          <a-button>
+            更多操作 <a-icon type="down" />
+          </a-button>
+        </a-dropdown>
+      </div>
+      <!-- 表格区域 -->
+      <a-table ref="TableInfo"
+               :columns="columns"
+               :dataSource="dataSource"
+               :pagination="pagination"
+               :loading="loading"
+               :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+               :scroll="{ x: 900 }"
+               @change="handleTableChange">
+        <template slot="email" slot-scope="text, record">
+          <a-popover placement="topLeft">
+            <template slot="content">
+              <div>{{text}}</div>
+            </template>
+            <p style="width: 150px;margin-bottom: 0">{{text}}</p>
+          </a-popover>
+        </template>
+        <template slot="operation" slot-scope="text, record">
+          <a-icon v-hasPermission="'user:update'" type="setting" theme="twoTone" twoToneColor="#4a9ff5" @click="edit(record)" title="修改用户"></a-icon>
+          &nbsp;
+          <a-icon v-hasPermission="'user:view'" type="eye" theme="twoTone" twoToneColor="#42b983" @click="view(record)" title="查看"></a-icon>
+          <a-badge v-hasNoPermission="'user:update','user:view'" status="warning" text="无权限"></a-badge>
+        </template>
+      </a-table>
+    </div>
+  </a-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
