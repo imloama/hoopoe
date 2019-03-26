4ffc4d24991068698d4e519b01d30fbdd9f55e42
max <imloama@gmail.com>
2019-03-26 22:16:50 +0800

    feat: tree and limit

--- core/src/main/java/hoopoe/core/tree/Tree.java
+++ core/src/main/java/hoopoe/core/tree/Tree.java
@@ -2,11 +2,15 @@ package hoopoe.core.tree;
 
 
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Tree<T extends ITree> implements ITree{
-    private List<Tree> children;
+    private List<Tree<T>> children;
     private T source;
+    private boolean leaf = true;// 是否叶子节点
 
     public Tree(T source){
         this.source = source;
@@ -14,29 +18,29 @@ public class Tree<T extends ITree> implements ITree{
 
     @Override
     public String getKey() {
-        return this.source.getKey();
+        return this.source == null ? "0" : this.source.getKey();
     }
 
     @Override
     public String getValue() {
-        return this.source.getValue();
+        return this.source == null ? "" : this.source.getValue();
     }
 
     @Override
     public String getLabel() {
-        return this.source.getLabel();
+        return this.source == null ? "" :this.source.getLabel();
     }
 
     @Override
     public String getParentKey() {
-        return this.source.getParentKey();
+        return this.source == null ? "" :this.source.getParentKey();
     }
 
-    public List<Tree> getChildren() {
+    public List<Tree<T>> getChildren() {
         return children;
     }
 
-    public void setChildren(List<Tree> children) {
+    public void setChildren(List<Tree<T>> children) {
         this.children = children;
     }
 
--- core/src/main/java/hoopoe/core/tree/TreeUtil.java
+++ core/src/main/java/hoopoe/core/tree/TreeUtil.java
@@ -1,14 +1,46 @@
 package hoopoe.core.tree;
 
+import com.google.common.collect.Lists;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class TreeUtil {
 
     private TreeUtil(){}
+    private final static String TOP_NODE_ID = "0";
 
-//    public static <T> Tree build(List<T> list){
-//
-//    }
+    public static <T extends ITree> Tree<T> build(List<T> list){
+        if(list == null)return null;
+        Map<String,Tree<T>> nodemap = list.stream()
+            .map(item -> new Tree<T>(item))
+            .collect(Collectors.toMap(Tree::getKey, tree -> tree));
+
+        List<String> topids = Lists.newArrayList();
+        nodemap.forEach((id, tree) -> {
+            if(tree.getParentKey() == null || TOP_NODE_ID.equals(tree.getParentKey())){
+                topids.add(id);
+                return;
+            }
+            Tree<T> parent = nodemap.get(tree.getParentKey());
+            List<Tree<T>> children = parent.getChildren();
+            if(children == null){
+               children = Lists.newArrayList();
+
+            }
+            children.add(tree);
+            parent.setChildren(children);
+        });
+        List<Tree<T>> tops = nodemap.entrySet().stream()
+                .filter( e -> topids.contains(e.getKey()))
+                .map(e -> e.getValue())
+                .collect(Collectors.toList());
+
+        Tree<T> root = new Tree<>(null);
+        root.setLeaf(false);
+        root.setChildren(tops);
+        return root;
+    }
 
 
 }
