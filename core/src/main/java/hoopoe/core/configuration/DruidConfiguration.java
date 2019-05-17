7c074c756932eb22c0131f9bed93c5e1a43f84af
max <imloama@gmail.com>
2019-05-17 21:52:50 +0800

    feat: update

--- core/src/main/java/hoopoe/core/configuration/DruidConfiguration.java
+++ core/src/main/java/hoopoe/core/configuration/DruidConfiguration.java
@@ -10,24 +10,24 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class DruidConfiguration implements WebMvcConfigurer {
-
-    @Bean
-    @Order
-    public ServletRegistrationBean statViewServlet() {
-        StatViewServlet servlet = new StatViewServlet();
-        ServletRegistrationBean bean = new ServletRegistrationBean(servlet, "/druid/*");
-        return bean;
-    }
-
-    @Bean
-    public FilterRegistrationBean webStatFilter(){
-        WebStatFilter filter = new WebStatFilter();
-        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
-        bean.addUrlPatterns("/*");
-        bean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.font,/druid/*");
-        return bean;
-    }
-
-}
+//@Configuration
+//public class DruidConfiguration implements WebMvcConfigurer {
+//
+//    @Bean
+//    @Order
+//    public ServletRegistrationBean statViewServlet() {
+//        StatViewServlet servlet = new StatViewServlet();
+//        ServletRegistrationBean bean = new ServletRegistrationBean(servlet, "/druid/*");
+//        return bean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean webStatFilter(){
+//        WebStatFilter filter = new WebStatFilter();
+//        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+//        bean.addUrlPatterns("/*");
+//        bean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.font,/druid/*");
+//        return bean;
+//    }
+//
+//}
